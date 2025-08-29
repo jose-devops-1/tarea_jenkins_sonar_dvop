1cddc2b518aba6e8435af4f11c914073ca469c06
jose-devops-1 <jose.ortega@outlook.cl>
2025-08-29 14:26:50 -0400

    workflow 1426

--- src/test/java/com/example/Tarea/TareaApplicationTests.java
+++ src/test/java/com/example/Tarea/TareaApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.Tarea;
 
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
 
-@SpringBootTest
-class TareaApplicationTests {
+public class TareaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    public void pruebaPaginaPrincipal() {
+        Configuration.headless = true; // Ejecuta sin abrir navegador .
+        open("http://localhost:8081/");
+        $("body").shouldHave(text("¡Aplicación operando correctamente! - DevOps -")); // Cambia "Bienvenido" por un texto de tu página
+    }
 }
